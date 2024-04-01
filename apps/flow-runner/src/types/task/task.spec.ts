f5356159bcc4321e49dd5aef6a3f70a2db5e3a72
intosKai <ivanoff.pg@ya.ru>
2024-04-01 22:39:13 +0300

    fix up task test

--- apps/flow-runner/src/types/task/task.spec.ts
+++ apps/flow-runner/src/types/task/task.spec.ts
@@ -1,7 +1,20 @@
-import { Task } from './task';
+import { v4 } from 'uuid';
+import { ChatGPTTask } from './task';
+import { TypeEnum } from '../../prisma';
 
 describe('Task', () => {
   it('should be defined', () => {
-    expect(new Task()).toBeDefined();
+    expect(
+      new ChatGPTTask({
+        id: v4(),
+        flowId: v4(),
+        type: TypeEnum.CHATGPT,
+        data: null,
+        result: null,
+        inputs: [v4()],
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }),
+    ).toBeDefined();
   });
 });
